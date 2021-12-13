cf2241173b43dcae2806af04c810f8a56aae0d56
Gao996 <51224634+Gao996@users.noreply.github.com>
2021-12-13 16:21:27 +0800

    Update util.js
    如果是文本类型的文件，文本里面包含了\r\n的数据，也会进行切片，变成多个buffer，并且\r\n会不见，所以这里把切掉的处理回来。

--- src/util.js
+++ src/util.js
@@ -2,106 +2,137 @@ const fs = require("fs");
 const path = require("path");
 
 Buffer.prototype.split = Buffer.prototype.split || function (spl) {
-  let arr = [];
-  let cur = 0;
-  let n = 0;
-  while ((n = this.indexOf(spl, cur)) != -1) {
-    arr.push(this.slice(cur, n));
-    cur = n + spl.length
-  }
-  arr.push(this.slice(cur))
-  return arr
+    let arr = [];
+    let cur = 0;
+    let n = 0;
+    while ((n = this.indexOf(spl, cur)) != -1) {
+        arr.push(this.slice(cur, n));
+        cur = n + spl.length
+    }
+    arr.push(this.slice(cur))
+    return arr
 }
 
 
 const getHtml = () => {
 
-  return fs.createReadStream(path.join(__dirname, "index.html"));
+    return fs.createReadStream(path.join(__dirname, "index.html"));
 
 }
 
 const fileHandler = (str, buffers, req) => {
 
-  let boundary = '--' + str.split('=')[1];
+    let boundary = '--' + str.split('=')[1];
 
-  //第一步使用boundary切割,形式类似于------WebKitFormBoundary8QtZlZVe7Tqym8tG
+    //第一步使用boundary切割,形式类似于------WebKitFormBoundary8QtZlZVe7Tqym8tG
 
-  let result = buffers.split(boundary);
+    let result = buffers.split(boundary);
 
-  //第二步丢弃首部的空元素和尾部的--元素
+    //第二步丢弃首部的空元素和尾部的--元素
 
-  result.shift();
-  result.pop();
-
-  /**
-   * 第三步现在只剩下两种形式
-   * 
-   * \r\n数据描述\r\n\r\n数据值\r\n 和 \r\n数据描述1\r\n数据描述2\r\n\r\n文件内容\r\n
-   * 
-   */
-  result = result.map(buffer => buffer.slice(2, buffer.length - 2))
-
-  /**
-   * 第四步 现在只剩下两种数据形式
-   * 
-   * 数据描述\r\n\r\n数据值 和 数据描述1\r\n数据描述2\r\n\r\n文件内容
-   * 
-   * 继续使用\r\n\r\n进行切割
-   */
-
-  result = result.map((buffer) => {
-    return buffer.split("\r\n\r\n");
-  })
-
-  /**
-   * 第五步要组合数据了
-   * 
-   */
-  let obj = {};
-
-  result.forEach((array) => {
+    result.shift();
+    result.pop();
 
     /**
-     *   普通表单数据格式
-     *   Content-Disposition: form-data; name="type_id"
-     *   1 
+     * 第三步现在只剩下两种形式
+     * 
+     * \r\n数据描述\r\n\r\n数据值\r\n 和 \r\n数据描述1\r\n数据描述2\r\n\r\n文件内容\r\n
+     * 
      */
-    if (array[0].indexOf("\r\n") == -1) { //普通表单数据
+    result = result.map(buffer => buffer.slice(2, buffer.length - 2))
 
-      const arr = array[0].toString().split(";");
-
-      let key = arr[arr.length - 1].split("=")[1].toString();
-
-      key = key.replace(/['"]/ig, "");
-
-      obj[key] = array[1].toString();
-
-    } else {//文件数据
-      /**
-       Content-Disposition: form-data; name="photo"; filename="854138674122619089.jpg"
-       Content-Type: image/jpeg
-
-       xxxxxxxxxxxxxx一堆二进制数据
+    /**
+     * 第四步 现在只剩下两种数据形式
+     * 
+     * 数据描述\r\n\r\n数据值 和 数据描述1\r\n数据描述2\r\n\r\n文件内容
+     * 
+     * 继续使用\r\n\r\n进行切割
      */
-      const fileData = array[1]; //承载二进制的文件数据
-      const arr = array[0].split("\r\n")[0].split(";");
-      let filename = arr[arr.length - 1].split("=")[1].toString();
-      filename = filename.replace(/['"]/ig, "");
-      const filePath = `/upload/${filename}`;
 
-      //现在要开始写文件了
-      fs.writeFile(path.join(__dirname, filePath), fileData, (err) => {
-        console.log(err);
-      })
+    result = result.map((buffer) => {
+        return buffer.split("\r\n\r\n");
+    })
 
-      obj.fileUrl = filePath;
+    /**
+     * 第五步要组合数据了
+     * 
+     */
+    let obj = {};
+
+    result.forEach((array) => {
+
+        /**
+         *   普通表单数据格式
+         *   Content-Disposition: form-data; name="type_id"
+         *   1 
+         */
+        if (array[0].indexOf("\r\n") == -1) { //普通表单数据
+
+            const arr = array[0].toString().split(";");
+
+            let key = arr[arr.length - 1].split("=")[1].toString();
+
+            key = key.replace(/['"]/ig, "");
+
+            obj[key] = array[1].toString();
+
+        } else {//文件数据
+            /**
+             Content-Disposition: form-data; name="photo"; filename="854138674122619089.jpg"
+             Content-Type: image/jpeg
+      
+             xxxxxxxxxxxxxx一堆二进制数据
+           */
+            const fileData = array[1]; //承载二进制的文件数据
+            const arr = array[0].split("\r\n")[0].split(";");
+            let filename = arr[arr.length - 1].split("=")[1].toString();
+            filename = filename.replace(/['"]/ig, "");
+            const filePath = `/upload/${filename}`;
+
+            //现在要开始写文件了
+            //fs.writeFile(path.join(__dirname, filePath), fileData, (err) => {
+            //  console.log(err);
+            //})
+
+            // TODO 对于文本文件存在\r\n的会进行切片，变成多个buffer，并且\r\n会不见，处理一手
+            var ans = 2;
+            if (array.length > ans) {
+                array.forEach(item => {
+
+                    if (ans == 2) {
+                        ans--;
+                    }
+                    else if (ans == 1) {
+                        fs.writeFileSync(path.join(__dirname, filePath), item + '\r\n\r\n', (err) => {
+                            console.error(err);
+                        })
+                        ans--;
+                    }
+                    else {
+                        // append 追加文件内容，需要同步写入
+                        fs.writeFileSync(path.join(__dirname, filePath), item + '\r\n\r\n', {
+                            flag: 'a', encoding: 'utf-8'
+                        }, (err) => {
+                            console.error(err);
+                        })
+                    }
+                })
+            }
+            else {
+                // writeFile 直接打开文件默认是 w 模式，所以如果文件存在，该方法写入的内容会覆盖旧的文件内容。
+                fs.writeFile(path.join(__dirname, filePath), fileData, (err) => {
+                    console.error(err);
+                })
+            }
+
+            obj.fileUrl = filePath;
 
 
-    }
+        }
 
-  })
+    })
 
-  return obj;
+    return obj;
 
 }
 
@@ -110,42 +141,42 @@ const fileHandler = (str, buffers, req) => {
  */
 const getPostData = (req) => {
 
-  return new Promise((resolve) => {
+    return new Promise((resolve) => {
 
-    let chunk = [];
+        let chunk = [];
 
-    req.on("data", (buffer) => {
-      chunk.push(buffer);
+        req.on("data", (buffer) => {
+            chunk.push(buffer);
+        })
+
+        req.on("end", () => {
+
+            const buffers = Buffer.concat(chunk);
+
+            let boundary = req.headers['content-type'].split('; ')[1]; //获取boundary分隔符,上传文件的时候在req的Content-Type中会带这个参数
+
+            if (boundary && boundary.includes("boundary")) { //触发文件下载
+                boundary = fileHandler(boundary, buffers, req); //将文件上传解析完毕的参数赋值给boundary
+            } else {
+                boundary = null;
+            }
+
+            if (boundary) { //文件上传
+
+                resolve(boundary);
+
+            } else { //普通post请求
+
+                let data = buffers.toString();
+
+                resolve(data);
+
+            }
+
+
+        })
+
     })
-
-    req.on("end", () => {
-
-      const buffers = Buffer.concat(chunk);
-
-      let boundary = req.headers['content-type'].split('; ')[1]; //获取boundary分隔符,上传文件的时候在req的Content-Type中会带这个参数
-
-      if (boundary && boundary.includes("boundary")) { //触发文件下载
-        boundary = fileHandler(boundary, buffers, req); //将文件上传解析完毕的参数赋值给boundary
-      } else {
-        boundary = null;
-      }
-
-      if (boundary) { //文件上传
-
-        resolve(boundary);
-
-      } else { //普通post请求
-
-        let data = buffers.toString();
-
-        resolve(data);
-
-      }
-
-
-    })
-
-  })
 
 }
 
@@ -154,24 +185,24 @@ const getPostData = (req) => {
  */
 const downloadFile = (pathUrl, res) => {
 
-  const readStream = fs.createReadStream(pathUrl);
+    const readStream = fs.createReadStream(pathUrl);
 
-  const stats = fs.statSync(pathUrl);
+    const stats = fs.statSync(pathUrl);
 
-  const filename = path.basename(pathUrl);
+    const filename = path.basename(pathUrl);
 
-  res.writeHead(200, {
-    'Content-Type': 'application/octet-stream', //告诉浏览器这是一个二进制文件
-    'Content-Disposition': 'attachment; filename=' + filename, //告诉浏览器这是一个需要下载的文件
-    'Content-Length': stats.size
-  });
+    res.writeHead(200, {
+        'Content-Type': 'application/octet-stream', //告诉浏览器这是一个二进制文件
+        'Content-Disposition': 'attachment; filename=' + filename, //告诉浏览器这是一个需要下载的文件
+        'Content-Length': stats.size
+    });
 
-  readStream.pipe(res);
+    readStream.pipe(res);
 
 }
 
 module.exports = {
-  getHtml,
-  getPostData,
-  downloadFile
+    getHtml,
+    getPostData,
+    downloadFile
 }
